d9e2b4bf65038657bc7e6179fce5734e26b55e24
phyllis likimani <likiphyllis@yahoo.com>
2022-12-10 12:35:34 -0600

    skipDefaultCheckout

--- skipDefaultCheckout/skipDefaultCheckout.groovy
+++ skipDefaultCheckout/skipDefaultCheckout.groovy
@@ -2,4 +2,4 @@
 // skip checking out code from source control by default in the agent directive (can be both pipeline and stage level)
 // it prevents the build from checking out the source code from the repository automatically
 
-
+// this will be a multipipeline job to make use of the build configuration which allows us to specify the location of the 
